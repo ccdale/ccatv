7b6f179ab44425b44ebfdc049a88c22cef95243b
ccdale <chris.charles.allison+git@gmail.com>
2026-05-29 18:31:13 +0100

    Add Flask bounce helper script

--- scripts/install-local-scripts.sh
+++ scripts/install-local-scripts.sh
@@ -8,5 +8,6 @@ TARGET_DIR="${HOME}/.local/bin"
 mkdir -p "${TARGET_DIR}"
 install -m 755 "${REPO_DIR}/scripts/ccatv-start" "${TARGET_DIR}/ccatv-start"
 install -m 755 "${REPO_DIR}/scripts/ccatv-stop" "${TARGET_DIR}/ccatv-stop"
+install -m 755 "${REPO_DIR}/scripts/ccatv-bounce-flask" "${TARGET_DIR}/ccatv-bounce-flask"
 
-echo "Installed ccatv-start and ccatv-stop to ${TARGET_DIR}"
+echo "Installed ccatv-start, ccatv-stop, and ccatv-bounce-flask to ${TARGET_DIR}"
